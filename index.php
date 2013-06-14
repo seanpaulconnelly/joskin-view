61326c1b9a9411f7bc196b3cb5bdc4ae46136b9a
Sean Connelly <sean@seanconnelly.me>
2013-06-13 22:22:31 -0700

    Basic homepage structure

--- index.php
+++ index.php
@@ -35,11 +35,71 @@
       </div>
     </header>
 
-      <div class="container">
-        <div class="row">
-          <h1>This is the title</h1>
-          <p class="skin lead">I carry a skin class based on the state of the button group above.</p>
+    <div class="container">
+
+      <div class="row banner">
+        <div class="span10">
+          ad
         </div>
+        <div class="span2 skin">
+          box
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="span12">
+          <h2>This is the title</h1>
+          <hr>
+          <p class="lead">This is the subtitle</p>
+        </div>
+      </div>
+
+      <div class="row events">
+        <div class="span4">
+          <h3>Junior Olympics</h3>
+          <p><strong>Start Date: </strong>xx/xx/xxxx</p>
+          <p><strong>End Date: </strong>xx/xx/xxxx</p>
+          <p><strong>Locations: </strong>Huntington, Irvine, Honda Center</p>
+          <p><strong>Streaming Games: </strong>Select Games &amp; Championships</p>
+          <a class="btn btn-large btn-green">View Event</a>
+        </div>
+        <div class="span4">
+          <h3>FIRS World Championships</h3>
+          <p><strong>Start Date: </strong>xx/xx/xxxx</p>
+          <p><strong>End Date: </strong>xx/xx/xxxx</p>
+          <p><strong>Locations: </strong>Huntington, Irvine, Honda Center</p>
+          <p><strong>Streaming Games: </strong>Select Games &amp; Championships</p>
+          <a class="btn btn-large btn-green">View Event</a>
+        </div>
+        <div class="span4">
+          <h3>AAU/USARS Adult Nationals</h3>
+          <p><strong>Start Date: </strong>xx/xx/xxxx</p>
+          <p><strong>End Date: </strong>xx/xx/xxxx</p>
+          <p><strong>Locations: </strong>Huntington, Irvine, Honda Center</p>
+          <p><strong>Streaming Games: </strong>Select Games &amp; Championships</p>
+          <a class="btn btn-large btn-green">View Event</a>
+        </div>
+      </div>
+      
+      <div class="row subfooter">
+        <div class="span3">
+          <h5>Title</h5>
+          <p>Lorem ipsum dolor sit amet bacon turducken mayonaise beef roast.  Saltines banana pudding Selena Gomez and the orange soda gang.</p>
+        </div>
+        <div class="span3">
+          <h5>Title</h5>
+          <p>Lorem ipsum dolor sit amet bacon turducken mayonaise beef roast.  Saltines banana pudding Selena Gomez and the orange soda gang.</p>
+        </div>
+        <div class="span3">
+          <h5>Title</h5>
+          <p>Lorem ipsum dolor sit amet bacon turducken mayonaise beef roast.  Saltines banana pudding Selena Gomez and the orange soda gang.</p>
+        </div>
+        <div class="span3">
+          <h5>Title</h5>
+          <p>Lorem ipsum dolor sit amet bacon turducken mayonaise beef roast.  Saltines banana pudding Selena Gomez and the orange soda gang.</p>
+        </div>
+      </div>
+
       </div><!-- /.container -->
 
       <footer>
