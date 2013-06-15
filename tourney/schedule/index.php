d62c7f28f0703ca07047e9d92c4ea51975cf1626
Sean Connelly <sean@seanconnelly.me>
2013-06-15 10:22:13 -0700

    Updating schedule view

--- tourney/schedule/index.php
+++ tourney/schedule/index.php
@@ -23,7 +23,7 @@ include '../../header.php'; ?>
 	</thead>
 	<tbody>
 		<tr>
-			<th>4920</th>
+			<td>4920</td>
 			<td>Jaguars vs. Gladiators</td>
 			<td>5 - 4</td>
 			<td>July 6th, 2013</td>
@@ -31,10 +31,10 @@ include '../../header.php'; ?>
 			<td>The Rink</td>
 			<td>LR1 / LR2</td>
 			<td>In Progress</td>
-			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">View Matchup</a></td>
 		</tr>
 		<tr>
-			<th>1048</th>
+			<td>1048</td>
 			<td>Eagles vs. Sharks</td>
 			<td>1 - 2</td>
 			<td>July 6th, 2013</td>
@@ -42,10 +42,10 @@ include '../../header.php'; ?>
 			<td>The Rink</td>
 			<td>LR1 / LR2</td>
 			<td>Played</td>
-			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">View Matchup</a></td>
 		</tr>
 		<tr>
-			<th>2934</th>
+			<td>2934</td>
 			<td>Robins vs. Red Deer Tour Snipers</td>
 			<td>3 - 8</td>
 			<td>July 6th, 2013</td>
@@ -53,10 +53,10 @@ include '../../header.php'; ?>
 			<td>The Rink</td>
 			<td>LR1 / LR2</td>
 			<td>Scheduled</td>
-			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">View Matchup</a></td>
 		</tr>
 		<tr>
-			<th>5012</th>
+			<td>5012</td>
 			<td>Verbero Swag of Ripon vs. Inline Sharks</td>
 			<td>2 - 1</td>
 			<td>July 6th, 2013</td>
@@ -64,10 +64,10 @@ include '../../header.php'; ?>
 			<td>The Rink</td>
 			<td>LR1 / LR2</td>
 			<td>In Progress</td>
-			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">View Matchup</a></td>
 		</tr>
 		<tr>
-			<th>6152</th>
+			<td>6152</td>
 			<td>Dangleitis vs. Dump and Run</td>
 			<td>9 - 2</td>
 			<td>July 6th, 2013</td>
@@ -75,10 +75,10 @@ include '../../header.php'; ?>
 			<td>The Rink</td>
 			<td>LR1 / LR2</td>
 			<td>In Progress</td>
-			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">View Matchup</a></td>
 		</tr>
 		<tr>
-			<th>2535</th>
+			<td>2535</td>
 			<td>San Jose Inline Sharks vs. Snipers</td>
 			<td>10 - 9</td>
 			<td>July 6th, 2013</td>
@@ -86,10 +86,10 @@ include '../../header.php'; ?>
 			<td>The Rink</td>
 			<td>LR1 / LR2</td>
 			<td>Scheduled</td>
-			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">View Matchup</a></td>
 		</tr>
 		<tr>
-			<th>7903</th>
+			<td>7903</td>
 			<td>Magic vs. Slight of Hand</td>
 			<td>7 - 6</td>
 			<td>July 6th, 2013</td>
@@ -97,10 +97,10 @@ include '../../header.php'; ?>
 			<td>The Rink</td>
 			<td>LR1 / LR2</td>
 			<td>Played</td>
-			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">View Matchup</a></td>
 		</tr>
 		<tr>
-			<th>4920</th>
+			<td>4920</td>
 			<td>Jaguars vs. Gladiators</td>
 			<td>5 - 4</td>
 			<td>July 6th, 2013</td>
@@ -108,10 +108,10 @@ include '../../header.php'; ?>
 			<td>The Rink</td>
 			<td>LR1 / LR2</td>
 			<td>In Progress</td>
-			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">View Matchup</a></td>
 		</tr>
 		<tr>
-			<th>1048</th>
+			<td>1048</td>
 			<td>Eagles vs. Sharks</td>
 			<td>1 - 2</td>
 			<td>July 6th, 2013</td>
@@ -119,10 +119,10 @@ include '../../header.php'; ?>
 			<td>The Rink</td>
 			<td>LR1 / LR2</td>
 			<td>Played</td>
-			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">View Matchup</a></td>
 		</tr>
 		<tr>
-			<th>2934</th>
+			<td>2934</td>
 			<td>Robins vs. Red Deer Tour Snipers</td>
 			<td>3 - 8</td>
 			<td>July 6th, 2013</td>
@@ -130,10 +130,10 @@ include '../../header.php'; ?>
 			<td>The Rink</td>
 			<td>LR1 / LR2</td>
 			<td>Played</td>
-			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">View Matchup</a></td>
 		</tr>
 		<tr>
-			<th>5012</th>
+			<td>5012</td>
 			<td>Verbero Swag of Ripon vs. Inline Sharks</td>
 			<td>2 - 1</td>
 			<td>July 6th, 2013</td>
@@ -141,10 +141,10 @@ include '../../header.php'; ?>
 			<td>The Rink</td>
 			<td>LR1 / LR2</td>
 			<td>Scheduled</td>
-			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">View Matchup</a></td>
 		</tr>
 		<tr>
-			<th>6152</th>
+			<td>6152</td>
 			<td>Dangleitis vs. Dump and Run</td>
 			<td>9 - 2</td>
 			<td>July 6th, 2013</td>
@@ -152,10 +152,10 @@ include '../../header.php'; ?>
 			<td>The Rink</td>
 			<td>LR1 / LR2</td>
 			<td>In Progress</td>
-			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">View Matchup</a></td>
 		</tr>
 		<tr>
-			<th>2535</th>
+			<td>2535</td>
 			<td>San Jose Inline Sharks vs. Snipers</td>
 			<td>10 - 9</td>
 			<td>July 6th, 2013</td>
@@ -163,10 +163,10 @@ include '../../header.php'; ?>
 			<td>The Rink</td>
 			<td>LR1 / LR2</td>
 			<td>Scheduled</td>
-			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">View Matchup</a></td>
 		</tr>
 		<tr>
-			<th>7903</th>
+			<td>7903</td>
 			<td>Magic vs. Slight of Hand</td>
 			<td>7 - 6</td>
 			<td>July 6th, 2013</td>
@@ -174,7 +174,7 @@ include '../../header.php'; ?>
 			<td>The Rink</td>
 			<td>LR1 / LR2</td>
 			<td>In Progress</td>
-			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">View Matchup</a></td>
 		</tr>
 	</tbody>
 </table>
