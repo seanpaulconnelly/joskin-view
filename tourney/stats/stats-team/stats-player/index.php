efac1ebec93f144e8d59c07f90f13265432e60a8
Sean Connelly <sean@seanconnelly.me>
2013-06-15 09:02:25 -0700

    Structure for the player view

--- tourney/stats/stats-team/stats-player/index.php
+++ tourney/stats/stats-team/stats-player/index.php
@@ -1,6 +1,89 @@
 <?php 
 include '../../../../header.php'; ?>
 
-Stats Player
+<ul class="breadcrumb">
+  <li><a href="<?php echo $siteurl; ?>">Home</a> <span class="divider">&rarr;</span></li>
+  <li><a href="<?php echo $siteurl; ?>/tourney">Junior Olympics</a> <span class="divider">&rarr;</span></li>
+  <li><a href="<?php echo $siteurl; ?>/tourney/stats">14U - A - Statistics</a> <span class="divider">&rarr;</span></li>
+  <li><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a> <span class="divider">&rarr;</span></li>
+  <li class="active">James Mastifson</li>
+</ul>
+
+<div class="row player-bio">
+	<div class="span2">
+		<img src="<?php echo $siteurl; ?>/assets/images/player.jpg" alt="Player Image"/>
+	</div>
+	<div class="span10">
+		<h2>James Mastifson</h2>
+		<p class="lead">Forward</p>
+		<p><strong>Red Deer Tour Snipers Record: </strong> 3-0-0</p>
+	</div>
+</div>
+
+<div class="row">
+	<div class="span12">
+		<table class="table table-bordered table-striped table-hover table-condensed">
+			<thead>
+				<tr>
+					<th>Game #</th>
+					<th>VS</th>
+					<th>Date</th>
+					<th>GP</th>
+					<th>G</th>
+					<th>A</th>
+					<th>PTS</th>
+					<th>PM</th>
+					<th>Result</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td><a href="<?php echo $siteurl; ?>/tourney/schedule/game-page">1432</a></td>
+					<td>Flyers</td>
+					<td>July 6th, 4:00PM</td>
+					<td>1</td>
+					<td>4</td>
+					<td>2</td>
+					<td>6</td>
+					<td>2:00</td>
+					<td>WIN 5-4</td>
+				</tr>
+				<tr>
+					<td><a href="<?php echo $siteurl; ?>/tourney/schedule/game-page">9203</a></td>
+					<td>Renegades</td>
+					<td>July 6th, 4:00PM</td>
+					<td>1</td>
+					<td>4</td>
+					<td>2</td>
+					<td>6</td>
+					<td>0:00</td>
+					<td>LOSS 2-1</td>
+				</tr>
+				<tr>
+					<td><a href="<?php echo $siteurl; ?>/tourney/schedule/game-page">5490</a></td>
+					<td>Smoky The Bears</td>
+					<td>July 6th, 4:00PM</td>
+					<td>1</td>
+					<td>4</td>
+					<td>2</td>
+					<td>6</td>
+					<td>0:00</td>
+					<td>LOSS 5-10</td>
+				</tr>
+				<tr>
+					<td><a href="<?php echo $siteurl; ?>/tourney/schedule/game-page">2468</a></td>
+					<td>WINNER OF <a href="<?php echo $siteurl; ?>/tourney/schedule/game-page">#3983</a></td>
+					<td>July 20th, 4:00PM</td>
+					<td>1</td>
+					<td>4</td>
+					<td>2</td>
+					<td>6</td>
+					<td>0:00</td>
+					<td>-</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
 
 <?php include '../../../../footer.php'; ?>
