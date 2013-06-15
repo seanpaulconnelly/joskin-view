7d275f5a75b74081a90f754101e7229d056857ba
Sean Connelly <sean@seanconnelly.me>
2013-06-14 18:46:23 -0700

    Structure for the game page

--- tourney/schedule/game-page/index.php
+++ tourney/schedule/game-page/index.php
@@ -1,6 +1,170 @@
 <?php 
 include '../../../header.php'; ?>
 
-Game Page
+<ul class="breadcrumb">
+  <li><a href="<?php echo $siteurl; ?>">Home</a> <span class="divider">&rarr;</span></li>
+  <li><a href="<?php echo $siteurl; ?>/tourney">2013 Junior Olympics</a> <span class="divider">&rarr;</span></li>
+  <li><a href="<?php echo $siteurl; ?>/tourney/schedule">Schedule</a> <span class="divider">&rarr;</span></li>
+  <li class="active">Game # 3649</li>
+</ul>
+
+<h2>This game is in progress.</h2>
+
+<div class="row matchup">
+	<div class="span5 away-team">
+		<div class="row">
+			<div class="span1">
+				logo
+			</div>
+			<div class="span3">
+				<h3>Red Deer Tour Snipers</h3>
+				<p>Record: 3-0-0</p>
+				<p>1st Place</p>
+			</div>
+			<div class="span1">
+				<p class="lead">5</p>
+			</div>
+		</div>
+	</div>
+	<div class="span2">
+		<p class="lead">vs.</p>
+	</div>
+
+	<div class="span5 home-team">
+		<div class="row">
+			<div class="span1">
+				<p class="lead">4</p>
+			</div>
+			<div class="span3">
+				<h3>Delta River Rats Orange</h3>
+				<p>Record: 3-0-0</p>
+				<p>3rd Place</p>
+			</div>
+			<div class="span1">
+				logo
+			</div>
+		</div>
+	</div>
+</div>
+
+<hr>
+<div class="row live-stream">
+	<div class="span7">
+		<a href="#"><img src="<?php echo $siteurl; ?>/assets/images/video.jpg" alt="Live Game Video"/></a>
+		<table class="table table-bordered table-striped table-hover table-condensed">
+			<thead>
+				<tr>
+					<th>Locker Rooms</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th>Red Deer Tour Snipers</th>
+					<td>LR1</td>
+				</tr>
+				<tr>
+					<th>Delta River Rats Orance</th>
+					<td>LR2</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
+	<div class="span4 offset1">
+		<table class="table table-bordered table-striped table-hover table-condensed">
+			<thead>
+				<tr>
+					<th>Box Score</th>
+					<th>1</th>
+					<th>2</th>
+					<th>OT</th>
+					<th>F</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Red Deer Tour Snipers</td>
+					<td>3</td>
+					<td>2</td>
+					<td>-</td>
+					<td>5</td>
+				</tr>
+				<tr>
+					<td>Delta River Rats Orange</td>
+					<td>2</td>
+					<td>2</td>
+					<td>-</td>
+					<td>4</td>
+				</tr>
+			</tbody>
+		</table>
+		<img src="<?php echo $siteurl; ?>/assets/images/block-ad.jpg" alt="300x250 Ad"/>
+	</div>
+</div>
+
+<div class="row action-summary">
+	<div class="span12">
+		<table class="table table-bordered table-striped table-hover table-condensed">
+			<thead>
+				<tr>
+					<th>Scoring Summary</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>01:37</td>
+					<td>Period 2</td>
+					<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+					<td>Scored By: <a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">#8 S. Connelly</a>, Assisted: <a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">#1 T. Chen</a></td>
+				</tr>
+				<tr>
+					<td>09:20</td>
+					<td>Period 1</td>
+					<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Delta River Rats Orange</a></td>
+					<td>Scored By: <a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">#8 K. Noll</a>, Assisted: <a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">#1 L. Geissler</a></td>
+				</tr>
+				<tr>
+					<td>03:38</td>
+					<td>Period 1</td>
+					<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+					<td>Scored By: <a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">#8 J. Noris</a>, Assisted: <a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">#1 G. Argyle</a></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+
+<div class="row action-summary">
+	<div class="span12">
+		<table class="table table-bordered table-striped table-hover table-condensed">
+			<thead>
+				<tr>
+					<th>Penalty Summary</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>01:37</td>
+					<td>Period 2</td>
+					<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+					<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">#8 S. Connelly</a>, Tripping</td>
+				</tr>
+				<tr>
+					<td>09:20</td>
+					<td>Period 1</td>
+					<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Delta River Rats Orange</a></td>
+					<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">#8 K. Noll</a>, Tripping</td>
+				</tr>
+				<tr>
+					<td>03:38</td>
+					<td>Period 1</td>
+					<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+					<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">#8 L. Geissler</a>, Tripping</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+
 
 <?php include '../../../footer.php'; ?>
