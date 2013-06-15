846e0a2ed00df72e284c066a0b9a700490cfbfa3
Sean Connelly <sean@seanconnelly.me>
2013-06-15 12:26:01 -0700

    Styling the homepage

--- index.php
+++ index.php
@@ -1,36 +1,37 @@
 <?php include 'header.php'; ?>
-      <div class="row">
+      <div class="row homepage-leader">
         <div class="span12">
-          <h2>This is the title</h1>
+          <h2>Get up to the minute information on the 2013 Junior Olympics of Inline Hockey, 
+FIRS World Championships, and AAU/USARS Adult Nationals.</h1>
           <hr>
-          <h3 class="lead">This is the subtitle</h3>
+          <h3 class="lead">This website will give you live updated schedules, stats, and standings during all 3 tournaments.  Select games will be broadcasted with live streaming video.  Choose an event below to get started.</h3>
         </div>
       </div>
 
       <div class="row events">
-        <div class="span4">
-          <h3>Junior Olympics</h3>
+        <div class="span4 event-box">
+          <h4>Junior Olympics</h4>
           <p><strong>Start Date: </strong>xx/xx/xxxx</p>
           <p><strong>End Date: </strong>xx/xx/xxxx</p>
           <p><strong>Locations: </strong>Huntington, Irvine, Honda Center</p>
           <p><strong>Streaming Games: </strong>Select Games &amp; Championships</p>
-          <a href="<?php echo $siteurl; ?>/tourney" class="btn btn-large btn-green">View Event</a>
+          <a href="<?php echo $siteurl; ?>/tourney" class="btn btn-large btn-green btn-block">View Event</a>
         </div>
-        <div class="span4">
-          <h3>FIRS World Championships</h3>
+        <div class="span4 event-box">
+          <h4>FIRS World Championships</h4>
           <p><strong>Start Date: </strong>xx/xx/xxxx</p>
           <p><strong>End Date: </strong>xx/xx/xxxx</p>
           <p><strong>Locations: </strong>Huntington, Irvine, Honda Center</p>
           <p><strong>Streaming Games: </strong>Select Games &amp; Championships</p>
-          <a href="<?php echo $siteurl; ?>/tourney" class="btn btn-large btn-green">View Event</a>
+          <a href="<?php echo $siteurl; ?>/tourney" class="btn btn-large btn-green btn-block">View Event</a>
         </div>
-        <div class="span4">
-          <h3>AAU/USARS Adult Nationals</h3>
+        <div class="span4 event-box">
+          <h4>AAU/USARS Adult Nationals</h4>
           <p><strong>Start Date: </strong>xx/xx/xxxx</p>
           <p><strong>End Date: </strong>xx/xx/xxxx</p>
           <p><strong>Locations: </strong>Huntington, Irvine, Honda Center</p>
           <p><strong>Streaming Games: </strong>Select Games &amp; Championships</p>
-          <a href="<?php echo $siteurl; ?>/tourney" class="btn btn-large btn-green">View Event</a>
+          <a href="<?php echo $siteurl; ?>/tourney" class="btn btn-large btn-green btn-block">View Event</a>
         </div>
       </div>
 <?php include 'footer.php'; ?>
