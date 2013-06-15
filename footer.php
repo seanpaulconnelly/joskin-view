cded1fb7e7d4992b433c9cfbcc7333eb5ec8984b
Sean Connelly <sean@seanconnelly.me>
2013-06-15 10:49:12 -0700

    Structure for the sub-footer

--- footer.php
+++ footer.php
@@ -1,4 +1,44 @@
       </div><!-- /.container -->
+      <div class="sub-footer">
+      	<div class="container">
+      		<div class="row">
+		      	<div class="span3">
+		      		<h5>Going to be at the event?</h5>
+		      		<p>Spectators will pay $25 at the door for each event.  Teams must be pre-registered by the required deadline.  More information can be found at these links:</p>
+		      		<ul>
+		      			<li><a href="#">Block Schedule</a></li>
+		      			<li><a href="#">AAU Hockey Website</a></li>
+		      			<li><a href="#">FIRS Website</a></li>
+		      		</ul>
+		      	</div>
+		      	<div class="span3">
+		      		<h5>What is RSportz?</h5>
+		      		<p>RSportz is a set of online tools used by sports organizations and facilities to manage all day to day interactions with their members, leagues, and tournaments.  A brand new platform will be available in August 2013.  See more here:</p>
+		      		<ul>
+		      			<li><a href="#">RSportz</a></li>
+		      		</ul>
+		      	</div>
+		      	<div class="span3">
+		      		<h5>What is Bleachers?</h5>
+		      		<p>Bleachers is a safe and secure service that provides live high definition streaming to your computer.  Watch video of varsity, intramural , and recreational athletics, concerts and performances, guest speakers and school events.</p>
+		      		<ul>
+		      			<li><a href="#">Bleachers</a></li>
+		      		</ul>
+		      	</div>
+		      	<div class="span3">
+		      		<h5>How do I learn more?</h5>
+		      		<p>If you'd like to reach out directly to one of the involved parties, contact information can be found here:</p>
+		      		<ul>
+		      			<li><a href="#">AAU National</a></li>
+		      			<li><a href="#">AAU Inline Hockey</a></li>
+		      			<li><a href="#">AAU Approved Hotels</a></li>
+		      			<li><a href="#">Bleachers</a></li>
+		      			<li><a href="#">RSportz</a></li>
+		      		</ul>
+		      	</div>
+		      </div>
+		  </div>
+      </div>
       <footer>
         <p>footer</p>
       </footer>
