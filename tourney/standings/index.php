19a419ea239f7fabbda8fd02213b5a36dca1f423
Sean Connelly <sean@seanconnelly.me>
2013-06-14 20:07:37 -0700

    Structure for the standings page

--- tourney/standings/index.php
+++ tourney/standings/index.php
@@ -1,6 +1,534 @@
 <?php 
 include '../../header.php'; ?>
 
-Standings
+<ul class="breadcrumb">
+  <li><a href="<?php echo $siteurl; ?>">Home</a> <span class="divider">&rarr;</span></li>
+  <li><a href="<?php echo $siteurl; ?>/tourney">2013 Junior Olympics</a> <span class="divider">&rarr;</span></li>
+  <li class="active">14U - A - Standings</li>
+</ul>
+
+<table class="table table-bordered table-striped table-hover table-condensed">
+	<thead>
+		<tr>
+			<td></td>
+			<th>Regular Season | Group A</th>
+			<th>GP</th>
+			<th>W</th>
+			<th>L</th>
+			<th>T</th>
+			<th>PTS</th>
+			<th>GD</th>
+			<th>GF</th>
+			<th>GA</th>
+			<th>PIM</th>
+			<th>WIN %</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>1</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+			<td>3</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>6</td>
+			<td>+10</td>
+			<td>10</td>
+			<td>0</td>
+			<td>2:00</td>
+			<td>100%</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Delta River Rats Orange</a></td>
+			<td>3</td>
+			<td>0</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>-10</td>
+			<td>0</td>
+			<td>10</td>
+			<td>4:00</td>
+			<td>0%</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+			<td>3</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>6</td>
+			<td>+10</td>
+			<td>10</td>
+			<td>0</td>
+			<td>2:00</td>
+			<td>100%</td>
+		</tr>
+		<tr>
+			<td>4</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Delta River Rats Orange</a></td>
+			<td>3</td>
+			<td>0</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>-10</td>
+			<td>0</td>
+			<td>10</td>
+			<td>4:00</td>
+			<td>0%</td>
+		</tr>
+	</tbody>
+</table>
+<hr>
+
+<table class="table table-bordered table-striped table-hover table-condensed">
+	<thead>
+		<tr>
+			<td></td>
+			<th>Regular Season | Group B</th>
+			<th>GP</th>
+			<th>W</th>
+			<th>L</th>
+			<th>T</th>
+			<th>PTS</th>
+			<th>GD</th>
+			<th>GF</th>
+			<th>GA</th>
+			<th>PIM</th>
+			<th>WIN %</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>1</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+			<td>3</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>6</td>
+			<td>+10</td>
+			<td>10</td>
+			<td>0</td>
+			<td>2:00</td>
+			<td>100%</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Delta River Rats Orange</a></td>
+			<td>3</td>
+			<td>0</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>-10</td>
+			<td>0</td>
+			<td>10</td>
+			<td>4:00</td>
+			<td>0%</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+			<td>3</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>6</td>
+			<td>+10</td>
+			<td>10</td>
+			<td>0</td>
+			<td>2:00</td>
+			<td>100%</td>
+		</tr>
+		<tr>
+			<td>4</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Delta River Rats Orange</a></td>
+			<td>3</td>
+			<td>0</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>-10</td>
+			<td>0</td>
+			<td>10</td>
+			<td>4:00</td>
+			<td>0%</td>
+		</tr>
+	</tbody>
+</table>
+<hr>
+
+<table class="table table-bordered table-striped table-hover table-condensed">
+	<thead>
+		<tr>
+			<td></td>
+			<th>Regular Season | Group C</th>
+			<th>GP</th>
+			<th>W</th>
+			<th>L</th>
+			<th>T</th>
+			<th>PTS</th>
+			<th>GD</th>
+			<th>GF</th>
+			<th>GA</th>
+			<th>PIM</th>
+			<th>WIN %</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>1</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+			<td>3</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>6</td>
+			<td>+10</td>
+			<td>10</td>
+			<td>0</td>
+			<td>2:00</td>
+			<td>100%</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Delta River Rats Orange</a></td>
+			<td>3</td>
+			<td>0</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>-10</td>
+			<td>0</td>
+			<td>10</td>
+			<td>4:00</td>
+			<td>0%</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+			<td>3</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>6</td>
+			<td>+10</td>
+			<td>10</td>
+			<td>0</td>
+			<td>2:00</td>
+			<td>100%</td>
+		</tr>
+		<tr>
+			<td>4</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Delta River Rats Orange</a></td>
+			<td>3</td>
+			<td>0</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>-10</td>
+			<td>0</td>
+			<td>10</td>
+			<td>4:00</td>
+			<td>0%</td>
+		</tr>
+	</tbody>
+</table>
+<hr>
+
+<table class="table table-bordered table-striped table-hover table-condensed">
+	<thead>
+		<tr>
+			<td></td>
+			<th>Regular Season | Group D</th>
+			<th>GP</th>
+			<th>W</th>
+			<th>L</th>
+			<th>T</th>
+			<th>PTS</th>
+			<th>GD</th>
+			<th>GF</th>
+			<th>GA</th>
+			<th>PIM</th>
+			<th>WIN %</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>1</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+			<td>3</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>6</td>
+			<td>+10</td>
+			<td>10</td>
+			<td>0</td>
+			<td>2:00</td>
+			<td>100%</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Delta River Rats Orange</a></td>
+			<td>3</td>
+			<td>0</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>-10</td>
+			<td>0</td>
+			<td>10</td>
+			<td>4:00</td>
+			<td>0%</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+			<td>3</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>6</td>
+			<td>+10</td>
+			<td>10</td>
+			<td>0</td>
+			<td>2:00</td>
+			<td>100%</td>
+		</tr>
+		<tr>
+			<td>4</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Delta River Rats Orange</a></td>
+			<td>3</td>
+			<td>0</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>-10</td>
+			<td>0</td>
+			<td>10</td>
+			<td>4:00</td>
+			<td>0%</td>
+		</tr>
+	</tbody>
+</table>
+<hr>
+
+<table class="table table-bordered table-striped table-hover table-condensed">
+	<thead>
+		<tr>
+			<td></td>
+			<th>Playoffs | Group A</th>
+			<th>GP</th>
+			<th>W</th>
+			<th>L</th>
+			<th>T</th>
+			<th>PTS</th>
+			<th>GD</th>
+			<th>GF</th>
+			<th>GA</th>
+			<th>PIM</th>
+			<th>WIN %</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>1</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+			<td>3</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>6</td>
+			<td>+10</td>
+			<td>10</td>
+			<td>0</td>
+			<td>2:00</td>
+			<td>100%</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Delta River Rats Orange</a></td>
+			<td>3</td>
+			<td>0</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>-10</td>
+			<td>0</td>
+			<td>10</td>
+			<td>4:00</td>
+			<td>0%</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+			<td>3</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>6</td>
+			<td>+10</td>
+			<td>10</td>
+			<td>0</td>
+			<td>2:00</td>
+			<td>100%</td>
+		</tr>
+		<tr>
+			<td>4</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Delta River Rats Orange</a></td>
+			<td>3</td>
+			<td>0</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>-10</td>
+			<td>0</td>
+			<td>10</td>
+			<td>4:00</td>
+			<td>0%</td>
+		</tr>
+		<tr>
+			<td>5</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+			<td>3</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>6</td>
+			<td>+10</td>
+			<td>10</td>
+			<td>0</td>
+			<td>2:00</td>
+			<td>100%</td>
+		</tr>
+		<tr>
+			<td>6</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Delta River Rats Orange</a></td>
+			<td>3</td>
+			<td>0</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>-10</td>
+			<td>0</td>
+			<td>10</td>
+			<td>4:00</td>
+			<td>0%</td>
+		</tr>
+	</tbody>
+</table>
+<hr>
+
+<table class="table table-bordered table-striped table-hover table-condensed">
+	<thead>
+		<tr>
+			<td></td>
+			<th>Playoffs | Group B</th>
+			<th>GP</th>
+			<th>W</th>
+			<th>L</th>
+			<th>T</th>
+			<th>PTS</th>
+			<th>GD</th>
+			<th>GF</th>
+			<th>GA</th>
+			<th>PIM</th>
+			<th>WIN %</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>1</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+			<td>3</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>6</td>
+			<td>+10</td>
+			<td>10</td>
+			<td>0</td>
+			<td>2:00</td>
+			<td>100%</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Delta River Rats Orange</a></td>
+			<td>3</td>
+			<td>0</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>-10</td>
+			<td>0</td>
+			<td>10</td>
+			<td>4:00</td>
+			<td>0%</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+			<td>3</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>6</td>
+			<td>+10</td>
+			<td>10</td>
+			<td>0</td>
+			<td>2:00</td>
+			<td>100%</td>
+		</tr>
+		<tr>
+			<td>4</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Delta River Rats Orange</a></td>
+			<td>3</td>
+			<td>0</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>-10</td>
+			<td>0</td>
+			<td>10</td>
+			<td>4:00</td>
+			<td>0%</td>
+		</tr>
+		<tr>
+			<td>5</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+			<td>3</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>6</td>
+			<td>+10</td>
+			<td>10</td>
+			<td>0</td>
+			<td>2:00</td>
+			<td>100%</td>
+		</tr>
+		<tr>
+			<td>6</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Delta River Rats Orange</a></td>
+			<td>3</td>
+			<td>0</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<td>-10</td>
+			<td>0</td>
+			<td>10</td>
+			<td>4:00</td>
+			<td>0%</td>
+		</tr>
+	</tbody>
+</table>
+<hr>
 
 <?php include '../../footer.php'; ?>
