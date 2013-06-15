3e2e44912f57e3ff8e57934eaa0e9f05e44cc281
Sean Connelly <sean@seanconnelly.me>
2013-06-14 17:30:02 -0700

    Structure for the schedule view

--- tourney/schedule/index.php
+++ tourney/schedule/index.php
@@ -1,6 +1,182 @@
 <?php 
 include '../../header.php'; ?>
 
-Schedule
+<ul class="breadcrumb">
+  <li><a href="<?php echo $siteurl; ?>">Home</a> <span class="divider">&rarr;</span></li>
+  <li><a href="<?php echo $siteurl; ?>/tourney">2013 Junior Olympics</a> <span class="divider">&rarr;</span></li>
+  <li class="active">14u - A - Schedule</li>
+</ul>
+
+<table class="table table-bordered table-striped table-hover table-condensed">
+	<thead>
+		<tr>
+			<th>Game #</th>
+			<th>Matchup</th>
+			<th>Score</th>
+			<th>Start</th>
+			<th>Location</th>
+			<th>Surface</th>
+			<th>Locker Room</th>
+			<th>Status</th>
+			<th>Link</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<th>4920</th>
+			<td>Jaguars vs. Gladiators</td>
+			<td>5 - 4</td>
+			<td>July 6th, 2013</td>
+			<td>Honda Center</td>
+			<td>The Rink</td>
+			<td>LR1 / LR2</td>
+			<td>In Progress</td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+		</tr>
+		<tr>
+			<th>1048</th>
+			<td>Eagles vs. Sharks</td>
+			<td>1 - 2</td>
+			<td>July 6th, 2013</td>
+			<td>Honda Center</td>
+			<td>The Rink</td>
+			<td>LR1 / LR2</td>
+			<td>Played</td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+		</tr>
+		<tr>
+			<th>2934</th>
+			<td>Robins vs. Red Deer Tour Snipers</td>
+			<td>3 - 8</td>
+			<td>July 6th, 2013</td>
+			<td>Honda Center</td>
+			<td>The Rink</td>
+			<td>LR1 / LR2</td>
+			<td>Scheduled</td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+		</tr>
+		<tr>
+			<th>5012</th>
+			<td>Verbero Swag of Ripon vs. Inline Sharks</td>
+			<td>2 - 1</td>
+			<td>July 6th, 2013</td>
+			<td>Honda Center</td>
+			<td>The Rink</td>
+			<td>LR1 / LR2</td>
+			<td>In Progress</td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+		</tr>
+		<tr>
+			<th>6152</th>
+			<td>Dangleitis vs. Dump and Run</td>
+			<td>9 - 2</td>
+			<td>July 6th, 2013</td>
+			<td>Honda Center</td>
+			<td>The Rink</td>
+			<td>LR1 / LR2</td>
+			<td>In Progress</td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+		</tr>
+		<tr>
+			<th>2535</th>
+			<td>San Jose Inline Sharks vs. Snipers</td>
+			<td>10 - 9</td>
+			<td>July 6th, 2013</td>
+			<td>Honda Center</td>
+			<td>The Rink</td>
+			<td>LR1 / LR2</td>
+			<td>Scheduled</td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+		</tr>
+		<tr>
+			<th>7903</th>
+			<td>Magic vs. Slight of Hand</td>
+			<td>7 - 6</td>
+			<td>July 6th, 2013</td>
+			<td>Honda Center</td>
+			<td>The Rink</td>
+			<td>LR1 / LR2</td>
+			<td>Played</td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+		</tr>
+		<tr>
+			<th>4920</th>
+			<td>Jaguars vs. Gladiators</td>
+			<td>5 - 4</td>
+			<td>July 6th, 2013</td>
+			<td>Honda Center</td>
+			<td>The Rink</td>
+			<td>LR1 / LR2</td>
+			<td>In Progress</td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+		</tr>
+		<tr>
+			<th>1048</th>
+			<td>Eagles vs. Sharks</td>
+			<td>1 - 2</td>
+			<td>July 6th, 2013</td>
+			<td>Honda Center</td>
+			<td>The Rink</td>
+			<td>LR1 / LR2</td>
+			<td>Played</td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+		</tr>
+		<tr>
+			<th>2934</th>
+			<td>Robins vs. Red Deer Tour Snipers</td>
+			<td>3 - 8</td>
+			<td>July 6th, 2013</td>
+			<td>Honda Center</td>
+			<td>The Rink</td>
+			<td>LR1 / LR2</td>
+			<td>Played</td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+		</tr>
+		<tr>
+			<th>5012</th>
+			<td>Verbero Swag of Ripon vs. Inline Sharks</td>
+			<td>2 - 1</td>
+			<td>July 6th, 2013</td>
+			<td>Honda Center</td>
+			<td>The Rink</td>
+			<td>LR1 / LR2</td>
+			<td>Scheduled</td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+		</tr>
+		<tr>
+			<th>6152</th>
+			<td>Dangleitis vs. Dump and Run</td>
+			<td>9 - 2</td>
+			<td>July 6th, 2013</td>
+			<td>Honda Center</td>
+			<td>The Rink</td>
+			<td>LR1 / LR2</td>
+			<td>In Progress</td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+		</tr>
+		<tr>
+			<th>2535</th>
+			<td>San Jose Inline Sharks vs. Snipers</td>
+			<td>10 - 9</td>
+			<td>July 6th, 2013</td>
+			<td>Honda Center</td>
+			<td>The Rink</td>
+			<td>LR1 / LR2</td>
+			<td>Scheduled</td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+		</tr>
+		<tr>
+			<th>7903</th>
+			<td>Magic vs. Slight of Hand</td>
+			<td>7 - 6</td>
+			<td>July 6th, 2013</td>
+			<td>Honda Center</td>
+			<td>The Rink</td>
+			<td>LR1 / LR2</td>
+			<td>In Progress</td>
+			<td><a class="btn btn-small btn-success" href="<?php echo $siteurl; ?>/tourney/schedule/game-page">Go To Game</a></td>
+		</tr>
+	</tbody>
+</table>
 
 <?php include '../../footer.php'; ?>
