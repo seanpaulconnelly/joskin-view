676571e2a8d03d744ca40b041e762c54bad2d530
Sean Connelly <sean@seanconnelly.me>
2013-06-15 10:09:14 -0700

    Structure for the stats view

--- tourney/stats/index.php
+++ tourney/stats/index.php
@@ -1,6 +1,292 @@
 <?php 
 include '../../header.php'; ?>
 
-Stats
+<ul class="breadcrumb">
+  <li><a href="<?php echo $siteurl; ?>">Home</a> <span class="divider">&rarr;</span></li>
+  <li><a href="<?php echo $siteurl; ?>/tourney">2013 Junior Olympics</a> <span class="divider">&rarr;</span></li>
+  <li class="active">14U - A - Statistics</li>
+</ul>
+
+<table class="table table-bordered table-striped table-hover table-condensed">
+	<thead>
+		<tr>
+			<td></td>
+			<th>Player</th>
+			<th>Team</th>
+			<th>POS</th>
+			<th>GP</th>
+			<th><a href="#">G</a></th>
+			<th><a href="#">A</a></th>
+			<th><a href="#">PTS <i class="icon-arrow-down"></i></th>
+			<th><a href="#">PM</a></th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>1</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">John James</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+			<td>Forward</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>6</th>
+			<td>2:00</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">James Mastifson</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Verbero Swag of Ripon</a></td>
+			<td>Defense</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>5</th>
+			<td>0:00</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">Drew Drury</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">San Jose Inline Sharks</a></td>
+			<td>Forward</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>4</th>
+			<td>4:00</td>
+		</tr>
+		<tr>
+			<td>4</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">Adam Adams</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Devil Dogs</a></td>
+			<td>Goalie</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<th>3</th>
+			<td>0:00</td>
+		</tr>
+		<tr>
+			<td>5</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">John James</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Tour Snipers</a></td>
+			<td>Forward</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>2</th>
+			<td>2:00</td>
+		</tr>
+		<tr>
+			<td>6</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">James Mastifson</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Devil Dogs</a></td>
+			<td>Defense</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>1</th>
+			<td>0:00</td>
+		</tr>
+		<tr>
+			<td>7</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">Drew Drury</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Devil Dogs</a></td>
+			<td>Forward</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>0</th>
+			<td>4:00</td>
+		</tr>
+		<tr>
+			<td>8</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">Adam Adams</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Tour Snipers</a></td>
+			<td>Goalie</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<th>0</th>
+			<td>0:00</td>
+		</tr>
+		<tr>
+			<td>9</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">John James</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+			<td>Forward</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>6</th>
+			<td>2:00</td>
+		</tr>
+		<tr>
+			<td>10</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">James Mastifson</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Verbero Swag of Ripon</a></td>
+			<td>Defense</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>5</th>
+			<td>0:00</td>
+		</tr>
+		<tr>
+			<td>11</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">Drew Drury</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">San Jose Inline Sharks</a></td>
+			<td>Forward</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>4</th>
+			<td>4:00</td>
+		</tr>
+		<tr>
+			<td>12</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">Adam Adams</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Devil Dogs</a></td>
+			<td>Goalie</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<th>3</th>
+			<td>0:00</td>
+		</tr>
+		<tr>
+			<td>13</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">John James</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Tour Snipers</a></td>
+			<td>Forward</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>2</th>
+			<td>2:00</td>
+		</tr>
+		<tr>
+			<td>14</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">James Mastifson</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Devil Dogs</a></td>
+			<td>Defense</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>1</th>
+			<td>0:00</td>
+		</tr>
+		<tr>
+			<td>15</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">Drew Drury</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Devil Dogs</a></td>
+			<td>Forward</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>0</th>
+			<td>4:00</td>
+		</tr>
+		<tr>
+			<td>16</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">Adam Adams</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Tour Snipers</a></td>
+			<td>Goalie</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<th>0</th>
+			<td>0:00</td>
+		</tr>
+		<tr>
+			<td>17</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">John James</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Red Deer Tour Snipers</a></td>
+			<td>Forward</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>6</th>
+			<td>2:00</td>
+		</tr>
+		<tr>
+			<td>18</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">James Mastifson</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Verbero Swag of Ripon</a></td>
+			<td>Defense</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>5</th>
+			<td>0:00</td>
+		</tr>
+		<tr>
+			<td>19</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">Drew Drury</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">San Jose Inline Sharks</a></td>
+			<td>Forward</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>4</th>
+			<td>4:00</td>
+		</tr>
+		<tr>
+			<td>20</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">Adam Adams</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Devil Dogs</a></td>
+			<td>Goalie</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<th>3</th>
+			<td>0:00</td>
+		</tr>
+		<tr>
+			<td>21</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">John James</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Tour Snipers</a></td>
+			<td>Forward</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>2</th>
+			<td>2:00</td>
+		</tr>
+		<tr>
+			<td>22</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">James Mastifson</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Devil Dogs</a></td>
+			<td>Defense</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>1</th>
+			<td>0:00</td>
+		</tr>
+		<tr>
+			<td>23</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">Drew Drury</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Devil Dogs</a></td>
+			<td>Forward</td>
+			<td>3</td>
+			<td>4</td>
+			<td>2</td>
+			<th>0</th>
+			<td>4:00</td>
+		</tr>
+		<tr>
+			<td>24</td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team/stats-player">Adam Adams</a></td>
+			<td><a href="<?php echo $siteurl; ?>/tourney/stats/stats-team">Tour Snipers</a></td>
+			<td>Goalie</td>
+			<td>3</td>
+			<td>0</td>
+			<td>0</td>
+			<th>0</th>
+			<td>0:00</td>
+		</tr>
+	</tbody>
+</table>
 
 <?php include '../../footer.php'; ?>
